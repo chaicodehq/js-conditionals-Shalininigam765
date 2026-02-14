8f61a9359ee4595f2d1c1e5c3381ee515ec46582
Shalini Nigam <Shalininigam764@gmail.com>
2026-02-15 00:44:30 +0530

    done 1st problem

--- src/01-ticket-pricing.js
+++ src/01-ticket-pricing.js
@@ -24,4 +24,59 @@
  */
 export function getTicketPrice(age, isWeekend) {
   // Your code here
+  const children = 0 <= age && age<= 12;
+  const Teens = 12 < age && age<= 17;
+  const Adults = 17 < age && age<= 59;
+  const Seniors = age >= 60;
+
+  if (!isWeekend) {
+      if (children) {
+          console.log("Ticket price is", 8);
+      }
+
+      else if (Teens) {
+          console.log("Ticket price is", 12);
+      }
+
+      else if (Adults) {
+          console.log("Ticket price is", 15);
+      }
+
+      else if (Seniors) {
+          console.log("Ticket price is", 10);
+      }
+
+      else if(age <0){
+        console.log(-1)
+      }
+
+  }
+
+  else if( isWeekend) {
+      if (children) {
+          console.log("Ticket price is $",(8+3));
+      }
+
+      else if (Teens) {
+          console.log("Ticket price is $", 12+3);
+      }
+
+      else if (Adults) {
+          console.log("Ticket price is", 3+15);
+      }
+
+      else if (Seniors) {
+          console.log("Ticket price is", 3+10);
+      }
+
+      else if(age <0){
+        console.log(-1)
+      }
+
+  }
+
 }
+
+getTicketPrice(30, true);
+getTicketPrice(14, false);
+getTicketPrice(-1,false);
